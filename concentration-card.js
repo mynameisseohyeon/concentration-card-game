e953cce48136df25716cd9259318fafcf385f5d2
seohyeon <uimi3606@gmail.com>
2023-01-22 20:31:54 +0900

    Add files via upload

--- concentration-card.js
+++ concentration-card.js
@@ -6,6 +6,7 @@ let colorCopy = colors.concat(colors); //colors를 2쌍으로 만들어줌 / con
 let shuffled = []; //배열
 let clicked = []; //클릭변수
 let completed = []; //완료변수
+let clickable = false; //효과 발생(카드 회전 시) 중 클릭되는 경우 막기
 
 function shuffle() { //랜덤으로 카드섞기
     for (let i = 0; colorCopy.length > 0; i += 1) {
@@ -31,6 +32,9 @@ function createCard(i) { //카드 생성
 };
 
 function onClickCard() { //클릭된 카드 검사
+    if (!clickable || completed.includes(this) || clicked[0]) { //처음에 뒤집히는 효과 나오는 동안 카드클릭, 이미 완성된 카드, 방금 클릭한 카드 한번 더 클릭되지 못하도록 설정
+        return; //함수를 중간에 멈춤
+    }
     this.classList.toggle('flipped');
     clicked.push(this);
     if(clicked.length !== 2) { //클릭된 카드가 2개가 아닐 시
@@ -42,6 +46,13 @@ function onClickCard() { //클릭된 카드 검사
         completed.push(clicked[0]); //클릭변수에서 완료변수로 옮긴다
         completed.push(clicked[1]);
         clicked = []; //클릭된 배열 초기화
+        if(completed.length !== total) { //완료된 카드가 tatal 즉, 12장이 아닌 경우
+            return;
+        }
+        setTimeout(() => {
+            alert('축하합니다!');
+            resetGame();
+        }, 1000);
         return; 
     };
     setTimeout(() => { //클릭된 두 카드가 일치하지 않을 경우
@@ -52,6 +63,7 @@ function onClickCard() { //클릭된 카드 검사
 }
 
 function startGame() { //게임시작
+    clickable = false;
     shuffle(); //카드섞기
     for (let i = 0; i < total; i += 1) {
         const card = createCard(i);
@@ -69,7 +81,17 @@ function startGame() { //게임시작
         document.querySelectorAll('.card').forEach((card) => {
             card.classList.remove('flipped');
         });
+        clickable = true;
     }, 5000);
 }
+
+function resetGame() { //게임 초기화
+    $wrapper.innerHTML = '';
+    colorCopy = colors.concat(colors); //colors와 같은 원본을 안 바꾸는 함수가 재사용이 쉽다
+    shuffled = [];
+    completed = [];
+    clickable = false;
+    startGame();
+};
 
 startGame();
